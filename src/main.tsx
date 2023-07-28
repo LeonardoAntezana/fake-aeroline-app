a58237312295ac06003c247c2903c68e8ee52a97
Leonardo <leonardoantezana59@gmail.com>
2023-07-28 16:10:23 -0300

    inicializando proyecto

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+)
+
+// AGREGAR EL REACT.STRICT MODE AL FINALIZAR EL PROYECTO, POR TEMAS DE SEGURIDAD Y BUENAS PRACTICAS
