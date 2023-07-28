5a2e77c7563ddb039562a115c0571e07f2c76234
Leonardo <leonardoantezana59@gmail.com>
2023-07-28 17:36:51 -0300

    create, styling and import first pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Suspense, lazy } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { privateRoutes, publicRoutes } from "./constants/public.routes.ts"
+
+const Home = lazy(() => import('./pages/Home/Home.tsx'))
+const Login = lazy(() => import('./pages/Login/Login.tsx'))
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Suspense fallback='Cargando'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Login} />
+          <Route path={publicRoutes.LOGIN} Component={Login} />
+          <Route path={privateRoutes.HOME} Component={Home} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   )
 }
 
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+import '../../sass/pages/Login.scss'
+
+const Login = () => {
+	return (
+		<div className="container__login">
+			Login
+		</div>
+	);
+};
+
+export default Login;
